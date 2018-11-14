35e3776a22ac54572bd6182c1cf43321f1308661
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-14 22:46:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
+function funkyFunction() {
+  return "FUNKY!";
+}
 
 var theFunk = funkyFunction();
 
